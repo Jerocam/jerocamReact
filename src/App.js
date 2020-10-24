2df3487349641213f5a73766ea4cb9d73686188b
jerocam <ocamposjero@gmail.com>
2020-10-24 12:24:03 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -50,42 +50,4 @@ class App extends Component {
   }
 }
 
-
-// function App() {
-
-//   return (
-    
-//     <div className="body-react">
-
-//       <Navbar/>
-
-//       <Routes/>
-      
-//       <div className="container alert alert-primary text-center" role="alert">
-//         <h5> Click <a href="https://github.com/Jerocam/jerocamReact" className="alert-link">the link</a> to get the source code from GitHub.</h5>
-//       </div>
-
-//       <footer className="page-footer font-small unique-color-dark darken-3 pt-3">
-//         <div className="container">
-//           <div className="row">
-//             <div className="col-md-12 mb-4">
-//               <p className="text-center">Visit my links</p>
-//               <div className="flex-center smedia mb-4">
-//                 <a href="https://angel.co/u/jeronimo-jose-ocampos" target="_blank" rel="noopener noreferrer"><FaAngellist /></a>
-//                 <a href="https://github.com/Jerocam" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-//                 <a href="https://www.linkedin.com/in/jerocam-226/" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="footer-copyright text-center py-3">&copy; 2020 Copyright - 
-//           <a href="https://jerocam.com/"> jerocam.com</a>
-//         </div>
-//       </footer>
-
-//     </div>
-    
-//   );
-// }
-
 export default App;
